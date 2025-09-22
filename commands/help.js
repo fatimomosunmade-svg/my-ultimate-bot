c0012875bf3a1a495031fad77ce6af9b0d21a442
Fatimo <fatimomosunmade@gmail.com>
2025-09-22 01:39:19 +0100

    Added .weather and .calc commands and updated help text

--- commands/help.js
+++ commands/help.js
@@ -4,9 +4,12 @@ module.exports = (bot, chatId) => {
 */help* - Show this help message
 */joke* - Tell a random joke
 */quote* - Share an inspirational quote
+*/weather* <city> - Get current weather for a city
+*/calc* <equation> - Calculate a math expression
 
 *You can also use a dot instead of a slash!*
-.example: .joke`;
+Example: .weather London
+Example: .calc 2+2`;
   // Use { parse_mode: 'Markdown' } to make the text look nicer
   bot.sendMessage(chatId, helpText, { parse_mode: 'Markdown' });
 };
